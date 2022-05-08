0fffb38f15250fd6e44ead2c40410a14cad24328
Francisco <franciscopadros8@gmail.com>
2022-05-08 18:46:30 -0300

    modificando navbar,falta hamburguesa

--- Jss/menuLateral.js
+++ Jss/menuLateral.js
@@ -1,7 +1,9 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
-
-navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("nav-menu_visible");
+addEventListener("DOMContentLoaded", () => {
+  const btn_menu = document.querySelector(".btn_menu");
+  if (btn_menu) {
+    btn_menu.addEventListener("click", () => {
+      const menu_items = document.querySelector("menu_items");
+      menu_items.classList.toggle("show");
+    });
+  }
 });
-
